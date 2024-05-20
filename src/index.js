003fa689513c6ecb87d35ed62b01d6e4827ca068
beesaiyvoud <laoseasybee@gmail.com>
2024-05-20 15:19:55 +0700

    add

--- src/index.js
+++ src/index.js
@@ -7,15 +7,12 @@ import router from "./router/index.js"
 import "colors"
 dotenv.config()
 connectMongoDB()
-
 const app = express();
 const PORT = process.env.PORT;
-
 app.use(core());
 app.use(bodyParser.json({ extended: false, limit: '500mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '500mb', parameterLimit: 50000 }));
 app.use(router)
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`.bgBlue)
 })
